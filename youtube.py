83346ea2e7927f531e512e5f9fbfb0c6a1d8bae3
Bradon Kanyid <rattboi@gmail.com>
2014-04-14 12:28:41 -0700

    Change to using urllib urlencode + a dict for the params

--- youtube.py
+++ youtube.py
@@ -1,18 +1,20 @@
 import requests
 import json
-import urllib2
+import urllib
 
 class Youtube(object):
     """ Support for looking up most relevant youtube video based on track name + artist """
 
     def get_link(self, track, artist):
         """ finds the video matching the current song """
-        query_base = "https://gdata.youtube.com/feeds/api/videos?orderby=relevance&start-index=1&max-results=1&v=2&alt=jsonc&q="
-        query = query_base + urllib2.quote(track) + "+" + urllib2.quote(artist)
+        query_base = "https://gdata.youtube.com/feeds/api/videos?%s"
+        yt_params = {'orderby': 'relevance', 'start-index': 1, 'max-results': 1, 'v': 2, 'alt': 'jsonc'}
+        yt_params.update(q='%s %s' % (track, artist))
+        query = query_base % urllib.urlencode(yt_params)
         r = requests.get(query)
         json_data = json.loads(r.text)
         yt_id = json_data['data']['items'][0]['id']
-        url = "https://youtu.be/" + yt_id
+        url = "https://youtu.be/%s" % yt_id
         if type(url) is unicode:
             url = url.encode("utf-8")
         return url
